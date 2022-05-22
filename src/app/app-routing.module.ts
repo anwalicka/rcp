eb04d774ba8f7212a55d85d238c920eeffbcfdde
Anna <you@example.com>
2022-05-22 21:44:52 +0200

    first commit - architecture - nav - sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppWrapperComponent } from './app-wrapper/app-wrapper.component';
+import { TaskComponent } from './views/task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppWrapperComponent },
+  { path: 'task', component: TaskComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { MainComponent } from './shared/main/main.component';
+import { TaskComponent } from './views/task/task.component';
+import { AppWrapperComponent } from './app-wrapper/app-wrapper.component';
+import { ViewWrapperComponent } from './view-wrapper/view-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MainComponent,
+    TaskComponent,
+    AppWrapperComponent,
+    ViewWrapperComponent
   ],
   imports: [
     BrowserModule,
